b7eff18966dc12b9e267664ad89dc28f1c8b3ab4
mluksevics <45032922+mluksevics@users.noreply.github.com>
2018-12-03 21:43:03 +0200

    Starting to rework all nodes
    Member release property added

---
+++ GH_RFEM/Properties_Hinge.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace GH_RFEM
+{
+    public class Properties_Hinge : GH_Component
+    {
+        
+        /// <summary>
+        /// Initializes a new instance of the Properties Hinge class.
+        /// </summary>
+        public Properties_Hinge()
+          : base("Member Hinge RFEM", "RFEM Member Hinge",
+              "Create relase to be applied for members",
+              "RFEM", "Properties")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        /// 
+        double uX = -1;
+        double uY = -1;
+        double uZ = -1;
+        double rX = -1;
+        double rY = -1;
+        double rZ = -1;
+        string Comment = "";
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            // to import lists or trees of values, modify the ParamAccess flag.
+            pManager.AddNumberParameter( "Spring X [N/m]", "uX", "set to -1=fixed (default), 0=free, other values create spring ", GH_ParamAccess.item, uX);
+            pManager.AddNumberParameter("Spring Y [N/m]", "uY", "set to 'true' if you want to release translation Y direction", GH_ParamAccess.item,uY);
+            pManager.AddNumberParameter("Spring Z [N/m]", "uZ", "set to 'true' if you want to release translation Z direction", GH_ParamAccess.item,uZ);
+            pManager.AddNumberParameter("Spring X rotation [Nm/rad]", "rX", "set to 'true' if you want to release rotation X direction", GH_ParamAccess.item,rX);
+            pManager.AddNumberParameter("Spring Y rotation [Nm/rad]", "rY", "set to 'true' if you want to release rotation Y direction", GH_ParamAccess.item,rY);
+            pManager.AddNumberParameter("Spring Z rotation [Nm/rad]", "rZ", "set to 'true' if you want to release rotation Z direction", GH_ParamAccess.item,rZ);
+            pManager.AddTextParameter("Comment", "Comment", "This text will be written in 'comments' parameter in RFEM data", GH_ParamAccess.item,Comment);
+
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        /// 
+        Dlubal.RFEM5.MemberHinge memberRelease = new Dlubal.RFEM5.MemberHinge();
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("RFEM Member Hinge definition", "Member Hinge", "RFEM member hinge definition for use with node that writes members", GH_ParamAccess.item);
+
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+
+
+            DA.GetData(0, ref uX);
+            DA.GetData(1, ref uY);
+            DA.GetData(2, ref uZ);
+            DA.GetData(3, ref rX);
+            DA.GetData(4, ref rY);
+            DA.GetData(5, ref rZ);
+            DA.GetData(6, ref Comment);
+
+            memberRelease.TranslationalConstantX = uX;
+            memberRelease.TranslationalConstantY = uY;
+            memberRelease.TranslationalConstantZ = uZ;
+            memberRelease.RotationalConstantX = rX;
+            memberRelease.RotationalConstantY = rY;
+            memberRelease.RotationalConstantZ = rZ;
+            memberRelease.Comment = Comment;
+
+            DA.SetData(0, memberRelease);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("3ef48063-2d29-4064-96ef-9d9d6844ba42"); }
+        }
+    }
+}
---
+++ GH_RFEM/Properties_Material_Cat.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Grasshopper.Kernel;
+using System.Windows.Forms;
+using Grasshopper.Kernel.Types;
+
+namespace BEST
+{
+    public class BestParameter : GH_PersistentParam<GH_Integer>
+    {
+        #region constructors
+        public BestParameter()
+                  : base("Material Catalogue", "Mat Catalogue",
+              "Select Material from catalogue",
+              "RFEM", "Test")
+        { }
+        public BestParameter(GH_InstanceDescription componentDescription)
+          : base(componentDescription)
+        { }
+        #endregion
+
+        #region properties
+        public static readonly Guid Id = new Guid("{1F421979-9529-49E1-8B9F-5F8285A25F15}");
+        public override Guid ComponentGuid
+        {
+            get { return Id; }
+        }
+        #endregion
+
+        #region constants
+        private static readonly Dictionary<int, string> _cities = new Dictionary<int, string>()
+    {
+      { 1, "Montgomery, Alabama" },
+      { 2, "Birmingham, Alabama" },
+      { 3, "Juneau, Alaska" },
+      { 4, "Anchorage, Alaska" },
+      { 5, "Phoenix, Arizona" },
+      { 6, "Tucson, Arizona" },
+      { 7, "Little Rock, Arkansas" },
+      { 8, "Fort Smith, Arkansas" },
+      { 9, "Olympia, Washington" },
+      { 10, "Seattle, Washington" },
+      { 11, "Madison, Wisconsin" },
+      { 12, "Milwaukee, Wisconsin" },
+      { 13, "Cheyenne, Wyoming" },
+      { 14, "Rock Springs, Wyoming" },
+      { 15, "Thermopolis, Wyoming" },
+      { 16, "Casper, Wyoming" },
+      { 17, "Sheridan, Wyoming" }
+    };
+        #endregion
+
+        #region menu
+        /// <summary>
+        /// Checks to see if a specific city code is currently the one and only integer in the volatile data.
+        /// </summary>
+        /// <param name="cityCode">City code to check.</param>
+        /// <returns>True if the city is currently selected.</returns>
+        private bool IsCitySelected(int cityCode)
+        {
+            if (SourceCount > 0) return false;
+            if (PersistentData.IsEmpty) return false;
+            if (PersistentDataCount != 1) return false;
+
+            GH_Integer integer = PersistentData.get_FirstItem(true);
+            return integer?.Value == cityCode;
+        }
+
+        protected override ToolStripMenuItem Menu_CustomSingleValueItem()
+        {
+            ToolStripMenuItem root = new ToolStripMenuItem("Pick State City");
+            if (SourceCount > 0)
+            {
+                root.Enabled = false;
+                return root;
+            }
+
+            ToolStripMenuItem[] stateItems = {
+        new ToolStripMenuItem("Alabama"),
+        new ToolStripMenuItem("Alaska"),
+        new ToolStripMenuItem("Arizona"),
+        new ToolStripMenuItem("Arkansas"),
+        new ToolStripMenuItem("Washington"),
+        new ToolStripMenuItem("Wisconsin"),
+        new ToolStripMenuItem("Wyoming")
+      };
+
+            foreach (KeyValuePair<int, string> pair in _cities)
+                InjectCity(stateItems, pair.Key, pair.Value);
+
+            foreach (ToolStripMenuItem item in stateItems)
+                root.DropDownItems.Add(item);
+
+            return root;
+        }
+        protected override ToolStripMenuItem Menu_CustomMultiValueItem()
+        {
+            return null;
+        }
+
+        private bool InjectCity(IEnumerable<ToolStripMenuItem> items, int code, string name)
+        {
+            int comma = name.IndexOf(",", StringComparison.Ordinal);
+            if (comma < 0)
+                throw new ArgumentException("Name must contain a comma with a state name.");
+
+            string state = name.Substring(comma + 1).Trim();
+            name = name.Substring(0, comma);
+
+            foreach (ToolStripMenuItem item in items)
+                if (item.Text.Equals(state, StringComparison.OrdinalIgnoreCase))
+                {
+                    ToolStripMenuItem cityItem = new ToolStripMenuItem(name);
+                    cityItem.Tag = code;
+                    cityItem.Checked = IsCitySelected(code);
+                    cityItem.ToolTipText = string.Format("{0}, {1}", name, state);
+                    cityItem.Click += CityItemOnClick;
+
+                    item.DropDownItems.Add(cityItem);
+                    return true;
+                }
+            return false;
+        }
+        private void CityItemOnClick(object sender, EventArgs eventArgs)
+        {
+            ToolStripMenuItem item = sender as ToolStripMenuItem;
+            if (item == null)
+                return;
+
+            int code = (int)item.Tag;
+            if (IsCitySelected(code))
+                return;
+
+            RecordPersistentDataEvent("Set city: " + item.Name);
+            PersistentData.Clear();
+            PersistentData.Append(new GH_Integer(code));
+            ExpireSolution(true);
+        }
+        #endregion
+
+        protected override GH_GetterResult Prompt_Singular(ref GH_Integer value)
+        {
+            return GH_GetterResult.cancel;
+        }
+        protected override GH_GetterResult Prompt_Plural(ref List<GH_Integer> values)
+        {
+            return GH_GetterResult.cancel;
+        }
+    }
+}

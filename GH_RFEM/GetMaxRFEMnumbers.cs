f18f6004bf7ee6c6981dfff5cfcfcbf3b96493ae
mluksevics <45032922+mluksevics@users.noreply.github.com>
2018-12-04 14:50:01 +0200

    Updated 'write node' . This version MUST have both comment and support as inputs.

---
+++ GH_RFEM/GetMaxRFEMnumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Dlubal.RFEM5;
+
+namespace GH_RFEM
+{
+    public class GetMaxNumber
+    {
+        public int NodeNumber(IModelData data)
+        {
+            int totalNodesCount = data.GetNodes().Count();
+            int lastNodeNo = data.GetNode(totalNodesCount-1, ItemAt.AtIndex).GetData().No;
+            return lastNodeNo;
+        }
+    }
+}
--- GH_RFEM/RFEM Write/Node_Write.cs
+++ GH_RFEM/RFEM Write/Node_Write.cs
@@ -14,9 +14,6 @@ namespace GH_RFEM
 {
     public class Node_Write : GH_Component
     {
-        //definition of RFEM variables used in further methods
-        IApplication app = null;
-        IModel model = null;
 
 
         /// <summary>
@@ -36,33 +33,40 @@ namespace GH_RFEM
         /// <summary>
         /// Registers all the input parameters for this component.
         /// </summary>
+        /// 
+
+        // We'll start by declaring input parameters and assigning them starting values.
+        List<Rhino.Geometry.Point3d> rhinoPointsInput = new List<Point3d>();
+        Dlubal.RFEM5.NodalSupport rfemNodalSupportInput = new Dlubal.RFEM5.NodalSupport();
+        string commentsInput = "";
+        bool run = false;
+
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
             // Use the pManager object to register your input parameters.
-            // You can often supply default values when creating parameters.
-            // All parameters must have the correct access type. If you want 
-            // to import lists or trees of values, modify the ParamAccess flag.
-            pManager.AddPointParameter("Point", "Point", "Input Rhino points you want to create as RFEM notes", GH_ParamAccess.list);
-            pManager.AddBooleanParameter("Run", "Toggle", "Toggles whether the nodes are written to RFEM", GH_ParamAccess.item, false);
+            pManager.AddPointParameter("Point", " Rhino Points", "Input Rhino points you want to create as RFEM notes", GH_ParamAccess.list);
+            pManager.AddGenericParameter("RFEM Support", "Support", "Leave empty if no support, use 'Nodal Support' node to generate support", GH_ParamAccess.item);
+            pManager.AddTextParameter("Text written in RFEM comments field", "Comment", "Text written in RFEM comments field", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("Run", "Toggle", "Toggles whether the nodes are written to RFEM", GH_ParamAccess.item, run);
 
-            // If you want to change properties of certain parameters, 
-            // you can use the pManager instance to access them by index:
-            //pManager[0].Optional = true;
+            pManager[1].Optional = true;
+            pManager[2].Optional = true;
         }
 
         /// <summary>
         /// Registers all the output parameters for this component.
         /// </summary>
+        /// 
+        //declaring output parameters
+        List<Dlubal.RFEM5.Node> RfemNodes = new List<Dlubal.RFEM5.Node>();
+        bool success = false;
+
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
             // Use the pManager object to register your output parameters.
-            // Output parameters do not have default values, but they too must have the correct access type.
+            pManager.AddGenericParameter("RFEM nodes", "RfNd", "Generated RFEM nodes", GH_ParamAccess.list);
+            pManager.AddBooleanParameter("Success", "Success", "Returns 'true' if nodes successfully writen, otherwise it is 'false'", GH_ParamAccess.item);
 
-            //pManager.AddGenericParameter("RFEM nodes", "RfNd", "RFEM nodes for export in RFEM", GH_ParamAccess.list);
-
-            // Sometimes you want to hide a specific parameter from the Rhino preview.
-            // You can use the HideParameter() method as a quick way:
-            //pManager.HideParameter(0);
         }
 
         /// <summary>
@@ -74,62 +78,105 @@ namespace GH_RFEM
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             // First, we need to retrieve all data from the input parameters.
-            // We'll start by declaring variables and assigning them starting values.
-            List<Rhino.Geometry.Point3d> rhino_points3d = new List<Point3d>();
-            bool run = false;
-            List<Dlubal.RFEM5.Node> RfemNodes = new List<Dlubal.RFEM5.Node>();
 
             // Then we need to access the input parameters individually. 
             // When data cannot be extracted from a parameter, we should abort this method.
-            if (!DA.GetDataList<Rhino.Geometry.Point3d>(0, rhino_points3d)) return;
-            DA.GetData(1,ref run);
+            if (!DA.GetDataList<Rhino.Geometry.Point3d>(0, rhinoPointsInput)) return;
+            DA.GetData(1, ref rfemNodalSupportInput);
+            DA.GetData(2, ref commentsInput);
+            DA.GetData(3, ref run);
 
             // The actual functionality will be in a method defined below. This is where we run it
             if (run == true)
             {
-                RfemNodes = CreateRfemNodes(rhino_points3d);
+                RfemNodes = CreateRfemNodes(rhinoPointsInput, rfemNodalSupportInput, commentsInput);
             }
 
             // Finally assign the processed data to the output parameter.
-            //DA.SetData(0, RfemNodes);
+            DA.SetDataList(0, RfemNodes);
+            DA.SetData(1, success);
+
+            rhinoPointsInput.Clear();
         }
 
-        private List<Dlubal.RFEM5.Node> CreateRfemNodes(List<Point3d> Rh_pt3d)
+        private List<Dlubal.RFEM5.Node> CreateRfemNodes(List<Point3d> Rh_pt3d, Dlubal.RFEM5.NodalSupport rfemNodalSupportMethodIn, string commentsListMethodIn)
         {
 
             // Gets interface to running RFEM application.
-            app = Marshal.GetActiveObject("RFEM5.Application") as IApplication;
+            IApplication app = Marshal.GetActiveObject("RFEM5.Application") as IApplication;
             // Locks RFEM licence
             app.LockLicense();
 
             // Gets interface to active RFEM model.
-            model = app.GetActiveModel();
+            IModel model = app.GetActiveModel();
 
             // Gets interface to model data.
             IModelData data = model.GetModelData();
 
+            // Gets Max node Number
+            int currentNewNodeNo = 1;
+            int totalNodesCount = data.GetNodes().Count();
+            if (totalNodesCount !=0)
+            {
+                int lastNodeNo = data.GetNode(totalNodesCount - 1, ItemAt.AtIndex).GetData().No;
+                currentNewNodeNo = lastNodeNo + 1;
+            }
+
+            // Gets Max nodal support number
+            int currentNewNodalSupportNo = 1;
+            int totalNodalSupportsCount = data.GetNodalSupports().Count();
+            if (totalNodalSupportsCount != 0)
+            {
+                int lastNodalSupportNo = data.GetNodalSupport(totalNodalSupportsCount - 1, ItemAt.AtIndex).GetData().No;
+                currentNewNodalSupportNo = lastNodalSupportNo + 1;
+            }
+
             //Create new array for RFEM point objects
             Dlubal.RFEM5.Node[] RfemNodeArray = new Dlubal.RFEM5.Node[Rh_pt3d.Count];
+            //Create a string for list with nodes
+            string createdNodesList ="";
 
-            ///This version writes nodes one-by-one because the data.SetNodes() for
+            ///This version writes nodes one-by-one, because the API method data.SetNodes() for
             ///array appears not to be working
             try
             {
-                // modification
-                // Sets all objects to model data.
-                 data.PrepareModification();
-                
-                for (int index = 0; index < Rh_pt3d.Count; index++)
+                // modification - Sets all objects to model data.
+                data.PrepareModification();
+
+
+                for (int i = 0; i < Rh_pt3d.Count; i++)
                 {
-                    RfemNodeArray[index].No = index+1;
-                    RfemNodeArray[index].X = Rh_pt3d[index].X;
-                    RfemNodeArray[index].Y = Rh_pt3d[index].Y;
-                    RfemNodeArray[index].Z = Rh_pt3d[index].Z;
-                    data.SetNode(RfemNodeArray[index]);
+                    RfemNodeArray[i].No = currentNewNodeNo;
+                    RfemNodeArray[i].X = Rh_pt3d[i].X;
+                    RfemNodeArray[i].Y = Rh_pt3d[i].Y;
+                    RfemNodeArray[i].Z = Rh_pt3d[i].Z;
+                    RfemNodeArray[i].Comment = commentsListMethodIn;
+
+                    data.SetNode(RfemNodeArray[i]);
+
+                    if (i== Rh_pt3d.Count - 1)
+                    {
+                        createdNodesList = createdNodesList + currentNewNodeNo.ToString();
+                    }
+                    else
+                    {
+                        createdNodesList = createdNodesList + currentNewNodeNo.ToString() + ",";
+                    }
+
+                    currentNewNodeNo++;
                 }
+
+                //addition of nodal supports
+                    rfemNodalSupportMethodIn.No = currentNewNodalSupportNo;
+                    rfemNodalSupportMethodIn.NodeList = createdNodesList;
+                    data.SetNodalSupport(ref rfemNodalSupportMethodIn);
+
 
                 // finish modification - RFEM regenerates the data
                 data.FinishModification();
+
+                //output 'success' as true 
+                success = true;
             }
 
             catch (Exception ex)
@@ -152,13 +199,10 @@ namespace GH_RFEM
             System.GC.WaitForPendingFinalizers();
 
             ///the lines below outputs created RFEM nodes in output parameter
-            ///current funcionality does not use this
-            ///it uses a custom class (written within this project) RfemNodeType to wrap the Dlubal.RFEM5.Node objects.
-            List<Dlubal.RFEM5.Node> RfemNodeList = RfemNodeArray.OfType<Dlubal.RFEM5.Node>().ToList(); // this isn't going to be fast.
+            List<Dlubal.RFEM5.Node> RfemNodeList = RfemNodeArray.OfType<Dlubal.RFEM5.Node>().ToList(); 
 
-            return RfemNodeList; 
+            return RfemNodeList;
 
-                            
         }
 
 

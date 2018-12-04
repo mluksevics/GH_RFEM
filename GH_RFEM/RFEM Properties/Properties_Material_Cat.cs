e65075a1c263eaf6607d0e4a5b77aba0f8456cff
mluksevics <45032922+mluksevics@users.noreply.github.com>
2018-12-04 11:50:22 +0200

    Material dropdown list added

--- GH_RFEM/RFEM Properties/Properties_Material_Cat.cs
+++ GH_RFEM/RFEM Properties/Properties_Material_Cat.cs
@@ -7,21 +7,21 @@ using Grasshopper.Kernel.Types;
 
 namespace BEST
 {
-    public class BestParameter : GH_PersistentParam<GH_Integer>
+    public class MaterialCatalogue : GH_PersistentParam<GH_String>
     {
         #region constructors
-        public BestParameter()
+        public MaterialCatalogue()
                   : base("Material Catalogue", "Mat Catalogue",
               "Select Material from catalogue",
-              "RFEM", "Test")
+              "RFEM", "Material")
         { }
-        public BestParameter(GH_InstanceDescription componentDescription)
+        public MaterialCatalogue(GH_InstanceDescription componentDescription)
           : base(componentDescription)
         { }
         #endregion
 
         #region properties
-        public static readonly Guid Id = new Guid("{1F421979-9529-49E1-8B9F-5F8285A25F15}");
+        public static readonly Guid Id = new Guid("{1e791e58-d0b8-4bfb-945b-e0b0f55c723c}");
         public override Guid ComponentGuid
         {
             get { return Id; }
@@ -29,25 +29,46 @@ namespace BEST
         #endregion
 
         #region constants
-        private static readonly Dictionary<int, string> _cities = new Dictionary<int, string>()
+        private static readonly Dictionary<string, string> _materials = new Dictionary<string, string>()
     {
-      { 1, "Montgomery, Alabama" },
-      { 2, "Birmingham, Alabama" },
-      { 3, "Juneau, Alaska" },
-      { 4, "Anchorage, Alaska" },
-      { 5, "Phoenix, Arizona" },
-      { 6, "Tucson, Arizona" },
-      { 7, "Little Rock, Arkansas" },
-      { 8, "Fort Smith, Arkansas" },
-      { 9, "Olympia, Washington" },
-      { 10, "Seattle, Washington" },
-      { 11, "Madison, Wisconsin" },
-      { 12, "Milwaukee, Wisconsin" },
-      { 13, "Cheyenne, Wyoming" },
-      { 14, "Rock Springs, Wyoming" },
-      { 15, "Thermopolis, Wyoming" },
-      { 16, "Casper, Wyoming" },
-      { 17, "Sheridan, Wyoming" }
+      { "NameID|Beton C12/15@TypeID|CONCRETE@NormID|EN 1992-1-1", "C12/15, Concrete (EN1992)" },
+      { "NameID|Beton C16/20@TypeID|CONCRETE@NormID|EN 1992-1-1", "C16/20, Concrete (EN1992)" },
+      { "NameID|Beton C20/25@TypeID|CONCRETE@NormID|EN 1992-1-1", "C20/25, Concrete (EN1992)" },
+      { "NameID|Beton C25/30@TypeID|CONCRETE@NormID|EN 1992-1-1", "C25/30, Concrete (EN1992)" },
+      { "NameID|Beton C28/35@TypeID|CONCRETE@NormID|BS EN 1992-1-1", "C28/35, Concrete (EN1992)" },
+      { "NameID|Beton C30/37@TypeID|CONCRETE@NormID|EN 1992-1-1", "C30/37, Concrete (EN1992)" },
+      { "NameID|Beton C32/40@TypeID|CONCRETE@NormID|BS EN 1992-1-1", "C32/40, Concrete (EN1992)" },
+      { "NameID|Beton C35/45@TypeID|CONCRETE@NormID|EN 1992-1-1", "C35/45, Concrete (EN1992)" },
+      { "NameID|Beton C40/50@TypeID|CONCRETE@NormID|EN 1992-1-1", "C40/50, Concrete (EN1992)" },
+      { "NameID|Beton C45/55@TypeID|CONCRETE@NormID|EN 1992-1-1", "C45/55, Concrete (EN1992)" },
+      { "NameID|Beton C50/60@TypeID|CONCRETE@NormID|EN 1992-1-1", "C50/60, Concrete (EN1992)" },
+      { "NameID|Beton C55/67@TypeID|CONCRETE@NormID|EN 1992-1-1", "C55/67, Concrete (EN1992)" },
+      { "NameID|Beton C60/75@TypeID|CONCRETE@NormID|EN 1992-1-1", "C60/75, Concrete (EN1992)" },
+      { "NameID|Beton C70/85@TypeID|CONCRETE@NormID|EN 1992-1-1", "C70/85, Concrete (EN1992)" },
+      { "NameID|Baustahl S 235@TypeID|STEEL@NormID|EN 1993-1-1", "S235, Steel (EN1993)" },
+      { "NameID|Baustahl S 275@TypeID|STEEL@NormID|EN 1993-1-1", "S275, Steel (EN1993)" },
+      { "NameID|Baustahl S 355@TypeID|STEEL@NormID|EN 1993-1-1", "S355, Steel (EN1993)" },
+      { "NameID|Baustahl S 450@TypeID|STEEL@NormID|EN 1993-1-1", "S355, Steel (EN1993)" },
+      { "NameID|Pappel und Nadelholz C14@TypeID|CONIFEROUS@NormID|EN 1995-1-1", "C14, Timber (EN1995)" },
+      { "NameID|Pappel und Nadelholz C16@TypeID|CONIFEROUS@NormID|EN 1995-1-1", "C16, Timber (EN1995)" },
+      { "NameID|Pappel und Nadelholz C18@TypeID|CONIFEROUS@NormID|EN 1995-1-1", "C18, Timber (EN1995)" },
+      { "NameID|Pappel und Nadelholz C20@TypeID|CONIFEROUS@NormID|EN 1995-1-1", "C20, Timber (EN1995)" },
+      { "NameID|Pappel und Nadelholz C22@TypeID|CONIFEROUS@NormID|EN 1995-1-1", "C22, Timber (EN1995)" },
+      { "NameID|Pappel und Nadelholz C24@TypeID|CONIFEROUS@NormID|EN 1995-1-1", "C24, Timber (EN1995)" },
+      { "NameID|Pappel und Nadelholz C27@TypeID|CONIFEROUS@NormID|EN 1995-1-1", "C27, Timber (EN1995)" },
+      { "NameID|Pappel und Nadelholz C30@TypeID|CONIFEROUS@NormID|EN 1995-1-1", "C30, Timber (EN1995)" },
+      { "NameID|Pappel und Nadelholz C35@TypeID|CONIFEROUS@NormID|EN 1995-1-1", "C35, Timber (EN1995)" },
+      { "NameID|Pappel und Nadelholz C40@TypeID|CONIFEROUS@NormID|EN 1995-1-1", "C40, Timber (EN1995)" },
+      { "NameID|Pappel und Nadelholz C45@TypeID|CONIFEROUS@NormID|EN 1995-1-1", "C45, Timber (EN1995)" },
+      { "NameID|Pappel und Nadelholz C50@TypeID|CONIFEROUS@NormID|EN 1995-1-1", "C50, Timber (EN1995)" },
+      { "NameID|Laubholz D18@TypeID|LEAFY@NormID|EN 1995-1-1", "D18, Timber (EN1995)" },
+      { "NameID|Laubholz D24@TypeID|LEAFY@NormID|EN 1995-1-1", "D24, Timber (EN1995)" },
+      { "NameID|Laubholz D30@TypeID|LEAFY@NormID|EN 1995-1-1", "D30, Timber (EN1995)" },
+      { "NameID|Laubholz D35@TypeID|LEAFY@NormID|EN 1995-1-1", "D35, Timber (EN1995)" },
+      { "NameID|Laubholz D40@TypeID|LEAFY@NormID|EN 1995-1-1", "D40, Timber (EN1995)" },
+      { "NameID|Laubholz D50@TypeID|LEAFY@NormID|EN 1995-1-1", "D50, Timber (EN1995)" },
+      { "NameID|Laubholz D60@TypeID|LEAFY@NormID|EN 1995-1-1", "D60, Timber (EN1995)" },
+      { "NameID|Laubholz D70@TypeID|LEAFY@NormID|EN 1995-1-1", "D70, Timber (EN1995)" },
     };
         #endregion
 
@@ -55,41 +76,37 @@ namespace BEST
         /// <summary>
         /// Checks to see if a specific city code is currently the one and only integer in the volatile data.
         /// </summary>
-        /// <param name="cityCode">City code to check.</param>
-        /// <returns>True if the city is currently selected.</returns>
-        private bool IsCitySelected(int cityCode)
+        /// <param name="materialCodeInput">Material code to check.</param>
+        /// <returns>True if the material is currently selected.</returns>
+        private bool IsMaterialSelected(string materialCodeInput)
         {
             if (SourceCount > 0) return false;
             if (PersistentData.IsEmpty) return false;
             if (PersistentDataCount != 1) return false;
 
-            GH_Integer integer = PersistentData.get_FirstItem(true);
-            return integer?.Value == cityCode;
+            GH_String materialCode = PersistentData.get_FirstItem(true);
+            return materialCode?.Value == materialCodeInput;
         }
 
         protected override ToolStripMenuItem Menu_CustomSingleValueItem()
         {
-            ToolStripMenuItem root = new ToolStripMenuItem("Pick State City");
+            ToolStripMenuItem root = new ToolStripMenuItem("Pick Material");
             if (SourceCount > 0)
             {
                 root.Enabled = false;
                 return root;
             }
 
-            ToolStripMenuItem[] stateItems = {
-        new ToolStripMenuItem("Alabama"),
-        new ToolStripMenuItem("Alaska"),
-        new ToolStripMenuItem("Arizona"),
-        new ToolStripMenuItem("Arkansas"),
-        new ToolStripMenuItem("Washington"),
-        new ToolStripMenuItem("Wisconsin"),
-        new ToolStripMenuItem("Wyoming")
+            ToolStripMenuItem[] materialTypeItems = {
+        new ToolStripMenuItem("Concrete (EN1992)"),
+        new ToolStripMenuItem("Steel (EN1993)"),
+        new ToolStripMenuItem("Timber (EN1995)"),
       };
 
-            foreach (KeyValuePair<int, string> pair in _cities)
-                InjectCity(stateItems, pair.Key, pair.Value);
+            foreach (KeyValuePair<string, string> pair in _materials)
+                InjectMaterial(materialTypeItems, pair.Key, pair.Value);
 
-            foreach (ToolStripMenuItem item in stateItems)
+            foreach (ToolStripMenuItem item in materialTypeItems)
                 root.DropDownItems.Add(item);
 
             return root;
@@ -99,11 +116,11 @@ namespace BEST
             return null;
         }
 
-        private bool InjectCity(IEnumerable<ToolStripMenuItem> items, int code, string name)
+        private bool InjectMaterial(IEnumerable<ToolStripMenuItem> items, string code, string name)
         {
             int comma = name.IndexOf(",", StringComparison.Ordinal);
             if (comma < 0)
-                throw new ArgumentException("Name must contain a comma with a state name.");
+                throw new ArgumentException("Name must contain a comma with a material type.");
 
             string state = name.Substring(comma + 1).Trim();
             name = name.Substring(0, comma);
@@ -111,39 +128,39 @@ namespace BEST
             foreach (ToolStripMenuItem item in items)
                 if (item.Text.Equals(state, StringComparison.OrdinalIgnoreCase))
                 {
-                    ToolStripMenuItem cityItem = new ToolStripMenuItem(name);
-                    cityItem.Tag = code;
-                    cityItem.Checked = IsCitySelected(code);
-                    cityItem.ToolTipText = string.Format("{0}, {1}", name, state);
-                    cityItem.Click += CityItemOnClick;
+                    ToolStripMenuItem materialItem = new ToolStripMenuItem(name);
+                    materialItem.Tag = code;
+                    materialItem.Checked = IsMaterialSelected(code);
+                    materialItem.ToolTipText = string.Format("{0}, {1}", name, state);
+                    materialItem.Click += MaterialItemOnClick;
 
-                    item.DropDownItems.Add(cityItem);
+                    item.DropDownItems.Add(materialItem);
                     return true;
                 }
             return false;
         }
-        private void CityItemOnClick(object sender, EventArgs eventArgs)
+        private void MaterialItemOnClick(object sender, EventArgs eventArgs)
         {
             ToolStripMenuItem item = sender as ToolStripMenuItem;
             if (item == null)
                 return;
 
-            int code = (int)item.Tag;
-            if (IsCitySelected(code))
+            string code = (string)item.Tag;
+            if (IsMaterialSelected(code))
                 return;
 
-            RecordPersistentDataEvent("Set city: " + item.Name);
+            RecordPersistentDataEvent("Set material: " + item.Name);
             PersistentData.Clear();
-            PersistentData.Append(new GH_Integer(code));
+            PersistentData.Append(new GH_String(code));
             ExpireSolution(true);
         }
         #endregion
 
-        protected override GH_GetterResult Prompt_Singular(ref GH_Integer value)
+        protected override GH_GetterResult Prompt_Singular(ref GH_String value)
         {
             return GH_GetterResult.cancel;
         }
-        protected override GH_GetterResult Prompt_Plural(ref List<GH_Integer> values)
+        protected override GH_GetterResult Prompt_Plural(ref List<GH_String> values)
         {
             return GH_GetterResult.cancel;
         }
